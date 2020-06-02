bc9b8febd6ebd0d571550f2cae2184bd6a6d16f5
Dmitry Vatutin <today.d89@gmail.com>
2020-06-02 15:57:19 +0300

    final fix

--- src/test/java/webdriver/page/AbstractPage.java
+++ src/test/java/webdriver/page/AbstractPage.java
@@ -6,7 +6,7 @@ public abstract class AbstractPage
 {
   protected WebDriver driver;
 
-  protected final int WAIT_TIMEOUT_SECONDS = 10; //TODO Switch to System -> properties -> wait_timeout in Framework
+  protected final int WAIT_TIMEOUT_SECONDS = 10;
 
   protected AbstractPage(WebDriver driver)
   {
